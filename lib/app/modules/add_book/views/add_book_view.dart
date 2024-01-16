f7799e2158fe127b6de698f4b5496d2f48ac9783
delandapp <alihandronikolas@gmail.com>
2024-01-16 09:46:03 +0700

    Add book

---
+++ lib/app/modules/add_book/views/add_book_view.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+import 'package:petugas_perpus_app/app/modules/add_book/controllers/add_book_controller.dart';
+
+
+
+class AddBookView extends GetView<AddBookController> {
+  const AddBookView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('AddBookView'),
+        centerTitle: true,
+      ),
+      body: Form(
+        key: controller.formKey,
+        child: Column(
+          children: [
+            TextFormField(
+              controller: controller.judulController,
+              decoration: InputDecoration(
+                hintText: "Masukkan Judul",
+              ),
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return "Judul Tidak Boleh Kosong";
+                }
+                return null;
+              },
+            ),
+            TextFormField(
+              controller: controller.penulisController,
+              //
+              decoration: InputDecoration(
+                hintText: "Masukkan penulis",
+              ),
+              
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return "penulis Tidak Boleh Kosong";
+                }
+                return null;
+              },
+            ),
+            TextFormField(
+              controller: controller.penerbitController,
+              //
+              decoration: InputDecoration(
+                hintText: "Masukkan penerbit",
+              ),
+              
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return "penerbit Tidak Boleh Kosong";
+                }
+                return null;
+              },
+            ),
+            TextFormField(
+              controller: controller.tahunController,
+              //
+              decoration: InputDecoration(
+                hintText: "Masukkan tahun terbit",
+              ),
+              
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return "tahun terbit Tidak Boleh Kosong";
+                }
+                return null;
+              },
+            ),
+            Obx(() => controller.loadingBook.value
+                ? CircularProgressIndicator()
+                : ElevatedButton(
+                    onPressed: () {
+                      controller.addbook();
+                    },
+                    child: Text("Tambah")))
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/app/modules/peminjaman/views/peminjaman_view.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+import 'package:petugas_perpus_app/app/routes/app_pages.dart';
+
+import '../controllers/peminjaman_controller.dart';
+
+class PeminjamanView extends GetView<PeminjamanController> {
+  const PeminjamanView({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('PeminjamanView'),
+        centerTitle: true,
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => Get.toNamed(Routes.ADD_BOOK),
+      ),
+      body: controller.obx((state) => ListView.separated(
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(state[index].user?.nama ?? "-"),
+            subtitle: Text('Buku yang dipinjam ${state[index].book?.judul ?? "-"}'),
+          );
+        },
+        separatorBuilder: (context, index) {
+          return Divider();
+        },
+        itemCount: state!.length,
+      )),
+    );
+  }
+}
